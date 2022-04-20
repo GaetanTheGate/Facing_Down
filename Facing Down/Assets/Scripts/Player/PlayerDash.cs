2d6a2bb2d446120890d17239ff1aed791443fe0e
Gaetan <gaetan.trinca-pupet@etu.univ-amu.fr>
2022-04-20 18:14:36 +0200

    Weapon Daggers

--- Facing Down/Assets/Scripts/Player/PlayerDash.cs
+++ Facing Down/Assets/Scripts/Player/PlayerDash.cs
@@ -40,8 +40,6 @@ public class PlayerDash : AbstractPlayer
     private void ComputeDash()
     {
         chargeTimePassed += Time.fixedDeltaTime;
-        if (stat.numberOfDashes >= stat.maxDashes)
-            return;
 
         if (Game.controller.IsMovementHeld() && !movePressed)
         {
@@ -51,26 +49,20 @@ public class PlayerDash : AbstractPlayer
         else if (!Game.controller.IsMovementHeld() && movePressed)
         {
             movePressed = false;
-            stat.numberOfDashes += 1;
 
             if (bulletTime.isInBulletTime)
             {
                 ComputeRedirect();
-
-                bulletTime.isInBulletTime = false;
-                Game.time.SetGameSpeedInstant(2.0f);
             }
             else
             {
                 if (chargeTimePassed > chargeTime)
                 {
                     ComputeMegaDash();
-                    Game.time.SetGameSpeedInstant(1.6f);
                 }
                 else
                 {
                     ComputeSimpleDash();
-                    Game.time.SetGameSpeedInstant(1.2f);
                 }
 
             }
@@ -82,16 +74,31 @@ public class PlayerDash : AbstractPlayer
 
     private void ComputeMegaDash()
     {
+        if (stat.numberOfDashes >= stat.maxDashes)
+            return;
+
+        stat.numberOfDashes += 2;
+        if (!bulletTime.isInBulletTime) Game.time.SetGameSpeedInstant(1.6f);
+
         self.GetComponent<Rigidbody2D>().velocity = new Velocity(stat.statEntity.acceleration * 2, pointer.getAngle()).GetAsVector2();
     }
 
     private void ComputeSimpleDash()
     {
+        if (stat.numberOfDashes >= stat.maxDashes)
+            return;
+
+        stat.numberOfDashes += 1;
+        if (!bulletTime.isInBulletTime) Game.time.SetGameSpeedInstant(1.2f);
+
         self.GetComponent<Rigidbody2D>().velocity = new Velocity(stat.statEntity.acceleration * 1.25f, pointer.getAngle()).GetAsVector2();
     }
 
     private void ComputeRedirect()
     {
+        bulletTime.isInBulletTime = false;
+        if (!bulletTime.isInBulletTime) Game.time.SetGameSpeedInstant(2.0f);
+
         Velocity newVelo = new Velocity(self.GetComponent<Rigidbody2D>().velocity);
         newVelo.setAngle(pointer.getAngle());
 
---
+++ Facing Down/Assets/Scripts/Weapons/WarAxe.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarAxe : Weapon
+{
+    public WarAxe()
+    {
+        baseAtk = 300;
+        baseRange = 3.5f;
+        baseLenght = 225;
+        baseSpan = 0.6f;
+        baseSDelay = 0.3f;
+        baseEDelay = 0.1f;
+        baseCooldown = 0.1f;
+
+        attackPath = "Prefabs/Weapons/Katana";
+        specialPath = "Prefabs/Weapons/Katana";
+    }
+
+    private SwingAttack.Way way = SwingAttack.Way.CounterClockwise;
+
+    public override void Attack(float angle, Entity self)
+    {
+        GameObject swing = GameObject.Instantiate(Resources.Load(attackPath, typeof(GameObject)) as GameObject);
+        swing.transform.position = self.transform.position;
+        swing.AddComponent<SwingAttack>();
+
+        swing.GetComponent<SwingAttack>().src = self;
+        swing.GetComponent<SwingAttack>().acceleration = 0.7f;
+        swing.GetComponent<SwingAttack>().angle = angle;
+        swing.GetComponent<SwingAttack>().range = baseRange;
+        swing.GetComponent<SwingAttack>().lenght = baseLenght;
+        swing.GetComponent<SwingAttack>().timeSpan = baseSpan;
+        swing.GetComponent<SwingAttack>().followEntity = true;
+        swing.GetComponent<SwingAttack>().startDelay = baseSDelay;
+        swing.GetComponent<SwingAttack>().endDelay = baseEDelay;
+
+        swing.GetComponent<SwingAttack>().way = way;
+
+        swing.GetComponent<SwingAttack>().startAttack();
+
+
+        if (way == SwingAttack.Way.Clockwise)
+            way = SwingAttack.Way.CounterClockwise;
+        else if (way == SwingAttack.Way.CounterClockwise)
+            way = SwingAttack.Way.Clockwise;
+    }
+
+    public override void Special(float angle, Entity self)
+    {
+        GameObject swing = GameObject.Instantiate(Resources.Load(attackPath, typeof(GameObject)) as GameObject);
+        swing.transform.position = self.transform.position;
+        swing.AddComponent<SwingAttack>();
+
+        swing.GetComponent<SwingAttack>().src = self;
+        swing.GetComponent<SwingAttack>().acceleration = 0.6f;
+        swing.GetComponent<SwingAttack>().angle = 90 - 10 / 2;
+        swing.GetComponent<SwingAttack>().range = baseRange;
+        swing.GetComponent<SwingAttack>().lenght = 10;
+        swing.GetComponent<SwingAttack>().timeSpan = 0.01f;
+        swing.GetComponent<SwingAttack>().startDelay = 1.0f;
+        swing.GetComponent<SwingAttack>().followEntity = true;
+
+        swing.GetComponent<SwingAttack>().way = SwingAttack.Way.Clockwise;
+
+        swing.GetComponent<SwingAttack>().onEndAttack += nextSpin;
+        swing.GetComponent<SwingAttack>().startAttack();
+    }
+
+    private void nextSpin(Entity self, float angle)
+    {
+        if (self.GetComponent<EntityCollisionStructure>().isGrounded)
+            return;
+
+        self.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        GameObject swing = GameObject.Instantiate(Resources.Load(attackPath, typeof(GameObject)) as GameObject);
+        swing.transform.position = self.transform.position;
+        swing.AddComponent<SwingAttack>();
+
+        swing.GetComponent<SwingAttack>().src = self;
+        swing.GetComponent<SwingAttack>().acceleration = 1f;
+        swing.GetComponent<SwingAttack>().angle = angle - 10;
+        swing.GetComponent<SwingAttack>().range = baseRange;
+        swing.GetComponent<SwingAttack>().lenght = 10;
+        swing.GetComponent<SwingAttack>().timeSpan = 0.01f;
+        swing.GetComponent<SwingAttack>().followEntity = true;
+
+        swing.GetComponent<SwingAttack>().way = SwingAttack.Way.Clockwise;
+
+        swing.GetComponent<SwingAttack>().onEndAttack += nextSpin;
+        swing.GetComponent<SwingAttack>().startAttack();
+    }
+}
