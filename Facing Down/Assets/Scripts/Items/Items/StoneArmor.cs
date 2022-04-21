06223e298c2ffa6c855b0b29b3dde738de98b13f
raphael lesbros <raphael.lesbros@gmail.com>
2022-04-21 14:06:18 +0200

    Ajout MagmaticCoating, StoneArmor

---
+++ Facing Down/Assets/Scripts/Items/Items/StoneArmor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneArmor : Item
+{
+	public StoneArmor() : base("StoneArmor", ItemRarity.UNCOMMON, ItemType.EARTH) { }
+
+	private readonly int maxHPAdd = 1;
+	public override void OnPickup() {
+		Game.player.stat.statEntity.hitPoints += maxHPAdd;
+	}
+
+	public override void OnRemove() {
+		Game.player.stat.statEntity.hitPoints -= maxHPAdd;
+	}
+
+	public override Item makeCopy() {
+		return new StoneArmor();
+	}
+}
