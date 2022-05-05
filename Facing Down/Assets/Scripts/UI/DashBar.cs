a937b94139b310df01ed5189675a4a5b35a76cce
raphael lesbros <raphael.lesbros@gmail.com>
2022-05-05 10:47:46 +0200

    Ajout DashBar

--- Facing Down/Assets/Scripts/UI/DashBar.cs
+++ Facing Down/Assets/Scripts/UI/DashBar.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DashBar : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int maxIcons;
+    public int xOffset;
+
+    private List<GameObject> dashIcons;
+    private int currentAvailableDashes;
+
+    private Sprite dashSprite;
+    private Sprite dashPlusSprite;
+    private GameObject dashIconPrefab;
+
+    /// <summary>
+    /// Initializes the prefab, icons and sprites;
+    /// </summary>
     void Start()
     {
-        
+        dashIconPrefab = Resources.Load<GameObject>("Prefabs/UI/DashIcon");
+        dashSprite = Resources.Load<Sprite>("UI/DashIcon");
+        dashPlusSprite = Resources.Load<Sprite>("UI/DashPlusIcon");
+
+        dashIcons = new List<GameObject>();
+        for (int i = 0; i < maxIcons; ++i) {
+            GameObject dashIcon = Instantiate<GameObject>(dashIconPrefab, transform);
+            dashIcon.transform.localPosition = new Vector2(xOffset * i, 0);
+            dashIcons.Add(dashIcon);
+		}
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateDashes()
     {
-        
+        int newAvailableDashes = Game.player.stat.maxDashes - Game.player.stat.numberOfDashes;
+        if (newAvailableDashes > currentAvailableDashes) {
+            for (int i = currentAvailableDashes; i < Mathf.Min(newAvailableDashes, dashIcons.Count); ++i) {
+                dashIcons[i].SetActive(true);
+			}
+            if (newAvailableDashes > maxIcons)
+                dashIcons[dashIcons.Count - 1].GetComponent<Image>().sprite = dashPlusSprite;
+
+        }
+        else if (newAvailableDashes < currentAvailableDashes) {
+            for (int i = newAvailableDashes; i < Mathf.Min(currentAvailableDashes, dashIcons.Count); ++i) {
+                dashIcons[i].SetActive(false);
+			}
+            if (newAvailableDashes <= maxIcons)
+                dashIcons[dashIcons.Count - 1].GetComponent<Image>().sprite = dashSprite;
+        }
+        currentAvailableDashes = newAvailableDashes;
     }
+
+	private void Update() {
+        UpdateDashes();
+	}
 }
