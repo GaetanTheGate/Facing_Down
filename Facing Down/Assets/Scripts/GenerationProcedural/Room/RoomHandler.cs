acb5d866a36cf390b5d5a6d35af5b85c12628d3f
Gaetan <gaetan.trinca-pupet@etu.univ-amu.fr>
2022-05-09 16:26:29 +0200

    RoomHandler Comment Start

--- Facing Down/Assets/Scripts/GenerationProcedural/Room/RoomHandler.cs
+++ Facing Down/Assets/Scripts/GenerationProcedural/Room/RoomHandler.cs
@@ -30,13 +30,13 @@ public class RoomHandler : MonoBehaviour
         OnEnterRoom();
         OnFinishRoom();
     }
-
+    /*
     private void Start()
     {
         InitRoom("test");
         AstarPath.active.Scan();
     }
-
+    */
     public void InitRoom(string category)
     {
         SetRoomInfo(category);
