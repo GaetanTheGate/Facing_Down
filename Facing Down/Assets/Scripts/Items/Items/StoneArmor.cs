3408ecb051f5edc18c786f83b41a796ce9e4e08b
raphael lesbros <raphael.lesbros@gmail.com>
2022-05-09 15:14:18 +0200

    Item Localization

--- Facing Down/Assets/Scripts/Items/Items/StoneArmor.cs
+++ Facing Down/Assets/Scripts/Items/Items/StoneArmor.cs
@@ -7,6 +7,11 @@ public class StoneArmor : Item
 	public StoneArmor() : base("StoneArmor", ItemRarity.UNCOMMON, ItemType.EARTH) { }
 
 	private readonly int maxHPAdd = 100;
+
+	public override string GetDescription() {
+		return string.Format(description.DESCRIPTION, maxHPAdd);
+	}
+
 	public override void OnPickup() {
 		Game.player.stat.ModifyMaxHP(maxHPAdd);
 		Game.player.stat.SetCurrentHP(Game.player.stat.GetCurrentHP() + maxHPAdd);
