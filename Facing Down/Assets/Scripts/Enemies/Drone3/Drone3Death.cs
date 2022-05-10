0b40f888c8b5332e4558653b13c62af0f6673e52
Quentin <quentin.roubin@etu.univ-amu.fr>
2022-05-10 17:50:20 +0200

    ajout de classes mères pour les "hit" "death" et "behaviour" des ennemis, améliorations du cyborg

--- Facing Down/Assets/Scripts/Enemies/Drone3/Drone3Death.cs
+++ Facing Down/Assets/Scripts/Enemies/Drone3/Drone3Death.cs
@@ -2,39 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drone3Death : MonoBehaviour
+public class Drone3Death : EnemyDeath
 {
-    public GameObject parentToDestroy;
 
-    private bool isDead = false;
-    private Animator animator;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        animator = gameObject.GetComponent<Animator>();
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (isDead)
-        {
-            StartCoroutine(startWaitingRoutine());
-        }
-    }
-
-    public void die()
-    {
-        GetComponent<GravityEntity>().gravity.setSpeed(9.8f);
-        if (Game.currentRoom != null && GetComponent<RoomHandler>() != null) Game.currentRoom.GetComponent<RoomHandler>().CheckIfRoomIsFinish();
-        BroadcastMessage("deathEvent");
-        isDead = true;
-    }
-
-    private IEnumerator startWaitingRoutine()
-    {
-        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
-        animator.SetBool("dead", true);
-        Destroy(parentToDestroy, animator.GetCurrentAnimatorStateInfo(0).length);
-    }
 }
---
+++ Facing Down/Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public int damage = 1;
+    protected bool isActive = true;
+
+    protected virtual void OnTriggerStay2D(Collider2D collision)
+    {
+        if (isActive)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                StatPlayer statPlayer = collision.GetComponent<StatPlayer>();
+                statPlayer.TakeDamage(new DamageInfo(gameObject.GetComponent<Entity>(), statPlayer.gameObject.GetComponent<Entity>(), damage));
+            }
+        }
+    }
+
+    protected virtual void deathEvent()
+    {
+        disableBehaviour();
+    }
+
+    protected void disableBehaviour()
+    {
+        isActive = false;
+        enabled = false;
+    }
+}
