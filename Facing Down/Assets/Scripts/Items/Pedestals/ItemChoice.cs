5b2feb00eec39a27ddeddbbde74fd8f06b6ef6df
raphael lesbros <raphael.lesbros@gmail.com>
2022-05-11 11:12:58 +0200

    ItemChoice -> nombre d'items variable

--- Facing Down/Assets/Scripts/Items/Pedestals/ItemChoice.cs
+++ Facing Down/Assets/Scripts/Items/Pedestals/ItemChoice.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class ItemChoice {
     private bool active;
-    private ItemPedestal firstItem;
-    private ItemPedestal secondItem;
+    private List<ItemPedestal> itemPedestals;
 
 
-    public ItemChoice(GameObject parent, Vector2 position1, Vector2 position2) {
+    public ItemChoice(GameObject parent, List<Vector2> positions) {
         active = true;
-        firstItem = ItemPedestal.SpawnRandomItemPedestal(parent, position1);
-        secondItem = ItemPedestal.SpawnRandomItemPedestal(parent, position2);
-        firstItem.SetItemChoice(this);
-        secondItem.SetItemChoice(this);
+        foreach (Vector2 position in positions) {
+            ItemPedestal itemPedestal = ItemPedestal.SpawnRandomItemPedestal(parent, position);
+            itemPedestal.SetItemChoice(this);
+            itemPedestals.Add(itemPedestal);
+		}
     }
 
     public void DisablePedestals() {
         if (!active) return;
         active = false;
-        firstItem.DisablePedestal();
-        secondItem.DisablePedestal();
+        foreach (ItemPedestal itemPedestal in itemPedestals) itemPedestal.DisablePedestal();
     }
 }
