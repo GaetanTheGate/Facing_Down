f72bf1baf7ecc7a598c36cce6a3a94fe2ee07b44
raphael lesbros <raphael.lesbros@gmail.com>
2022-05-11 11:32:35 +0200

    Ajout nom à l'itemPreview

--- Facing Down/Assets/Scripts/UI/ItemPreview.cs
+++ Facing Down/Assets/Scripts/UI/ItemPreview.cs
@@ -7,18 +7,21 @@ public class ItemPreview : MonoBehaviour
 {
     private Image itemIcon;
     private Text itemDescription;
+    private Text itemName;
     private Item currentItem;
 
     void Start()
     {
-        itemIcon = GetComponentInChildren<Image>();
-        itemDescription = GetComponentInChildren<Text>();
+        itemIcon = transform.Find("Icon").GetComponent<Image>();
+        itemDescription = transform.Find("Description").GetComponent<Text>();
+        itemName = transform.Find("Name").GetComponent<Text>();
     }
 
     public void SetItem(Item item) {
         currentItem = item;
         itemIcon.sprite = item.GetSprite();
         itemDescription.text = item.GetDescription();
+        itemName.text = item.GetName();
 	}
 
     public Item GetCurrentItem() {
