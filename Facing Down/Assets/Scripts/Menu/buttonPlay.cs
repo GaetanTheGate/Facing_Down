cc6cefda289e290a002c04a548b4ecf20b907ea4
Thomas-DaPrato <thomas.DA-PRATO@etu.univ-amu.fr>
2022-05-12 11:09:08 +0200

    menu finish

---
+++ Facing Down/Assets/Scripts/Menu/buttonPlay.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonPlay : MonoBehaviour
+{
+    public void printYolo(){
+        print("yolo");
+    }
+}
