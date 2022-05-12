3ec3928533d94a8b68b9e2e2c68ffcc8c2604406
raphael lesbros <raphael.lesbros@gmail.com>
2022-05-12 09:10:40 +0200

    Ajout bacjground à ItemPreview

--- Facing Down/Assets/Scripts/Items/Pedestals/ItemChoice.cs
+++ Facing Down/Assets/Scripts/Items/Pedestals/ItemChoice.cs
@@ -22,4 +22,8 @@ public class ItemChoice {
         active = false;
         foreach (ItemPedestal itemPedestal in itemPedestals) itemPedestal.DisablePedestal();
     }
+
+    public List<ItemPedestal> GetItemPedestals() {
+        return itemPedestals;
+    }
 }
