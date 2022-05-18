101d85fe9b7414863241cce0d2faa9b815a93ccb
Thomas-DaPrato <thomas.DA-PRATO@etu.univ-amu.fr>
2022-05-18 16:01:39 +0200

    link sliders volume

--- Facing Down/Assets/Scripts/Menu/MenuManager.cs
+++ Facing Down/Assets/Scripts/Menu/MenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using System.IO;
 
 public class MenuManager : MonoBehaviour
@@ -8,8 +9,13 @@ public class MenuManager : MonoBehaviour
     public static GameObject gameObjectActions;
     public static GameObject gameObjectOptions;
 
+    public AudioMixer audioMixerEditor;
+    public static AudioMixer audioMixer;
+
     void Awake(){
         Localization.Init();
+
+        audioMixer = audioMixerEditor;
 
         gameObjectActions = GameObject.Find("Actions");
         gameObjectOptions = GameObject.Find("Options");
@@ -27,6 +33,10 @@ public class MenuManager : MonoBehaviour
     }
 
     public static void applyOptions(){
+
+        audioMixer.SetFloat("masterVolume", Options.Get().masterVolumeValue);
+        audioMixer.SetFloat("musicVolume", Options.Get().musicVolumeValue);
+        audioMixer.SetFloat("soundVolume", Options.Get().soundVolumeValue);
         
     }
 }
