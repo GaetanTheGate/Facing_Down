9c84cd009647b879e893143d8c9668ef130cb7f3
Gaetan <gaetan.trinca-pupet@etu.univ-amu.fr>
2022-05-19 23:56:56 +0200

    Cyber spritesheet + lights

---
+++ Facing Down/Assets/Resources/animations/Items/Pedestals/ThunderLights.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThunderLights : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log(animator.gameObject.name);
+        animator.GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>().enabled = false;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.gameObject.GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>().enabled = true;
+    }
+}
