801d572ab40d3b68bb7c05262d6a8959aa48dbef
Quentin <quentin.roubin@etu.univ-amu.fr>
2022-06-06 16:58:15 +0200

    ajout du nouveau sprite du shield, et amélioration visuelle de la rotation

--- Facing Down/Assets/Scripts/Enemies/ArmoredCyborg/ArmoredCyborgMovement.cs
+++ Facing Down/Assets/Scripts/Enemies/ArmoredCyborg/ArmoredCyborgMovement.cs
@@ -9,6 +9,7 @@ public class ArmoredCyborgMovement : EnemyMovement
     //private ArmoredCyborgAttack armoredCyborgAttack;
     private EntityCollisionStructure entityCollisionStructure;
     private bool isAtBorder;
+    private bool isShieldFlipped;
 
     // Start is called before the first frame update
     public override void Start()
@@ -26,6 +27,7 @@ public class ArmoredCyborgMovement : EnemyMovement
         nextFlag = flags[0];
         rangeFromPlayerMax = 1.5f;
         isAtBorder = false;
+        isShieldFlipped = false;
     }
 
     public override void FixedUpdate()
@@ -113,6 +115,15 @@ public class ArmoredCyborgMovement : EnemyMovement
             gameObject.transform.Find("ShieldPivot_y").localRotation = Quaternion.Euler(0, 180, 0);
         else
             gameObject.transform.Find("ShieldPivot_y").localRotation = Quaternion.Euler(0, 0, 0);
+
+        Transform shieldTransform = gameObject.transform.Find("ShieldPivot_y").Find("ShieldPivot_z").Find("Shield");
+
+        if ( (gameObject.transform.Find("ShieldPivot_y").Find("ShieldPivot_z").localRotation.z > 0.5f && !isShieldFlipped) || 
+            (gameObject.transform.Find("ShieldPivot_y").Find("ShieldPivot_z").localRotation.z <= 0.5f && isShieldFlipped) )
+        {
+            shieldTransform.localScale = new Vector3(shieldTransform.localScale.x, shieldTransform.localScale.y * -1, shieldTransform.localScale.z);
+            isShieldFlipped = !isShieldFlipped;
+        } 
     }
 
     public void childTriggerExitGround()
